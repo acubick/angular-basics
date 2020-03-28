527d23d7dd28b035fab2428bb4d6ea0e8d1bf839
acubick <acubick@gmail.com>
2020-03-28 19:31:45 +0200

    test3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  git = "запись для фиксации  Git перед стартом видео";
+  git = "запись для фиксации  Git перед стартом видео" // нигде не используется
   title = "angular-unit-tests";
 }
