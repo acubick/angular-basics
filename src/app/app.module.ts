ca96ab379333c1e962eea0b531fcf0aa36e16633
acubick <acubick@gmail.com>
2020-03-29 17:49:47 +0300

    Integration tests finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import {AppComponent} from './app.component';
 import {CounterComponent} from "./counter/counter.component";
 import {FormsModule} from "@angular/forms";
 import {PostsComponent} from "./posts/posts.component";
+import { RoutingComponent } from './routing/routing.component';
+import {RouterModule} from "@angular/router";
+import { NavbarComponent } from './navbar/navbar.component';
+import { ColorDirective } from './directives/color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    PostsComponent
+    PostsComponent,
+    RoutingComponent,
+    NavbarComponent,
+    ColorDirective,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
