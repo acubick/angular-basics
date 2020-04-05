dedac9986026a09e6338817feb59ba36a4ea9534
acubick <acubick@gmail.com>
2020-04-05 16:48:58 +0300

    старт блока httpclient
    Took 6 minutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms'
-import { MyValidators } from './my.validators'
-
+import {Component, OnInit} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  ngOnInit(): void {
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+
   }
-
-
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
 
-import { AppComponent } from './app.component';
-
-
+import {AppComponent} from './app.component'
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,9 +10,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
